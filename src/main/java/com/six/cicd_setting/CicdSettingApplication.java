e22673407be8ecffa2617eba547445eb756b6474
si eun <tldms123455@naver.com>
2024-03-30 16:47:08 +0900

    first commit

---
+++ src/main/java/com/six/cicd_setting/CicdSettingApplication.java
@@ -0,0 +1,13 @@
+package com.six.cicd_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdSettingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdSettingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/six/cicd_setting/CicdSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six.cicd_setting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSettingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
