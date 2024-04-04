1551750c4940656f694b5b151e5dd1f581f70ee9
kudongku <kudongku@naver.com>
2024-04-04 12:23:18 +0900

    :sparkles: feat(s3): image upload 기능 추가

--- src/main/java/com/six/cicd_setting/HiController.java
+++ src/main/java/com/six/cicd_setting/HiController.java
@@ -1,16 +1,21 @@
 package com.six.cicd_setting;
 
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class HiController {
 
     private final HiService hiService;
+    private final S3Service s3Service;
 
     @GetMapping("/helloWorld/{hiStr}")
     public String createHi(
@@ -27,9 +32,16 @@ public class HiController {
     }
 
     @GetMapping("/health")
-    @ResponseBody
     public String healthCheck() {
         return "ok";
+    }
+
+    @PostMapping("/s3")
+    public String s3FileUpload(
+        MultipartFile image
+    ) throws IOException {
+        System.out.println(s3Service.upload(image, "image-test"));
+        return "uploaded";
     }
 
 }
